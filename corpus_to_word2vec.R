7ff048c0592ee0c7deb3b54b6c0d20e4c44db2ea
Andreas Blätte <andreasblatte@MBP-von-Andreas.fritz.box>
2022-06-18 17:09:09 +0200

    w2v recipe before Rmd

--- corpus_to_word2vec.R
+++ corpus_to_word2vec.R
@@ -1,20 +1,56 @@
+---
+title: "Train word2vec model"
+output: html_document
+date: '2022-06-18'
+---
+
+## Objective
+  
+
+
+## Dependencies / packages uses
+
+```{r}
 library(polmineR) # v0.8.6.9004
 library(wordVectors) # devtools::install_github("bmschmidt/wordVectors")
 library(readr)
-library(pbapply)
 library(parallel)
+```
 
-# alternative: https://cran.r-project.org/web/packages/word2vec
+Note on the packages used:
+  
+- [word2vec](https://CRAN.R-project.org/package=word2vec) is an alternative to
+the [wordVectors](https://github.com/bmschmidt/wordVectors) package used here, 
+but we think that wordVectors is very usable and well maintained.
+
+- The [readr](https://CRAN.R-project.org/package=readr) package is impressively
+efficient to write a big character to disk.
+
+
+# Settings
+
+```{r}  
+corpus_id <- "REUTERS"
+split_by <- "article_article_id"
 
 file_out <- "~/Lab/tmp/sz.txt"
 vectors_bin <- "~/Lab/tmp/sz.bin"
 cores <- detectCores() - 1L
+```
 
-corpus("NADIRASZ") %>%
-  split(s_attribute = "article_article_id", mc = cores, progress = TRUE) %>% 
-  get_token_stream(p_attribute = "word", progress = TRUE, collapse = " ", boost = TRUE) %>%
+## Write corpus data to disk
+
+```{r}
+corpus(corpus_id) %>%
+  split(s_attribute = split_by, mc = cores, progress = TRUE) %>% 
+  get_token_stream(p_attribute = "word", progress = TRUE, collapse = " ") %>%
   write_lines(file = file_out)
+```
 
+
+## Train word2vec model
+
+```{r}
 train_word2vec(
   file_out,
   vectors_bin,
@@ -24,3 +60,8 @@ train_word2vec(
   iter = 5,
   negative_samples = 0
 )
+```
+
+## Result
+
+
